0f5aa161d3981b8ae572afaad9ac35f4cd7444b0
HuzaiFa A <huziafarazaa999@gmail.com>
2026-06-02 01:36:09 +0500

    navbar responsive done

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -214,21 +214,21 @@ const Navbar = () => {
   return (
     <>
       <nav
-        className="w-full h-full bg-[#0D1717] relative flex flex-col justify-center items-center"
+        className="w-full h-svh bg-[#0D1717] relative flex flex-col justify-center items-center"
         id="Navbar"
         ref={navRef}
       >
         <div
-          className="w-full flex justify-between items-center h-24 px-4 z-100 fixed top-0 border-b font-Glitten-Regular tracking-widest text-[#FDFCDC] border-dashed backdrop-blur-sm"
+          className="w-full flex justify-between items-center py-3 mobile:py-5 px-4 z-100 fixed top-0 border-b font-Glitten-Regular tracking-widest text-[#FDFCDC] bg-[#0D1717] border-dashed backdrop-blur-sm"
           id="Navbar_Top"
         >
-          <h1 className="text-lg sm:text-3xl">CAR X</h1>
+          <h1 className="text-lg tablet:text-3xl">CAR X</h1>
           <button
             className="flex flex-col items-center justify-center cursor-pointer"
             onClick={nav_O_C_Handler}
           >
-            <HiDotsHorizontal className="size-8 sm:size-10" />
-            <div className="text-sm sm:text-lg relative inline-block overflow-hidden">
+            <HiDotsHorizontal className="size-8 tablet:size-10" />
+            <div className="text-sm tablet:text-lg relative inline-block overflow-hidden">
               <span className={`block Nav_Menu_Open`}>Open</span>
               <span className={`absolute left-0 top-full Nav_Menu_Close`}>
                 Close
@@ -243,7 +243,7 @@ const Navbar = () => {
           onMouseMove={nav_Center_Circle_Handler}
         >
           <div
-            className={`absolute left-0 w-[40%] h-full flex items-center justify-center`}
+            className={`absolute left-0 w-full mobile:w-[40%] h-full flex items-center justify-center`}
           >
             {Array.from({ length: 5 }, (_, i) => i + 1).map((elem, index) => {
               return (
@@ -251,7 +251,7 @@ const Navbar = () => {
                   <img
                     src={`/images/nav-image-${elem}.webp`}
                     alt={`Nav_Image_${elem}`}
-                    className={`object-contain object-center absolute ${nav_Link_Circle_Text === "Hey!" ? `${index < 4 && "opacity-0"}` : elem === nav_Link_Circle_Text ? "w-137.5 h-162.5 opacity-100" : "w-50 h-50 opacity-0"}`}
+                    className={`object-cover mobile:object-contain object-center absolute ${nav_Link_Circle_Text === "Hey!" ? `${index < 4 && "opacity-0"}` : elem === nav_Link_Circle_Text ? "w-full h-full mobile:w-87.5 mobile:h-87.5 tablet:w-112.5 tablet:h-112.5 desktop:w-137.5 desktop:h-162.5 opacity-100" : "w-50 h-50 opacity-0 animate-bounce"}`}
                     id="Nav_Images"
                     onMouseMove={nav_Images_Animation}
                   />
@@ -260,18 +260,29 @@ const Navbar = () => {
             })}
           </div>
 
-          <div className="absolute right-0 w-[60%] h-full text-[#fdfcdc] flex flex-col justify-center items-start gap-15  font-extrabold px-10 text-6xl">
+          <div
+            className="absolute right-0 w-full mobile:w-[60%] h-full text-[#fdfcdc] flex flex-col justify-center items-start gap-4 laptop:gap-7 desktop:gap-10 extraLarge:gap-11 font-extrabold px-2 z-20
+          bg-black/60 mobile:bg-transparent
+          "
+          >
             {Array.from({ length: 4 }, (_, i) => i + 1).map((elem, index) => {
               return (
                 <React.Fragment key={index}>
                   <li
-                    className="flex justify-center items-center gap-5 cursor-pointer py-2 z-1"
+                    className="flex justify-center items-center gap-5 cursor-pointer py-2  p-2"
                     onMouseEnter={() => {
                       nav_Link_Circle_Animation(elem);
                     }}
+                    onTouchStart={() => {
+                      nav_Link_Circle_Animation(elem);
+                    }}
                   >
-                    <span className="text-xl">{elem}.</span>
-                    <a target="_blank" href="https://www.github.com/huzaifarz">
+                    <span className="text-lg extraLarge:text-xl">{elem}.</span>
+                    <a
+                      target="_blank"
+                      href="https://www.github.com/huzaifarz"
+                      className="text-[22px] smallMobile:text-[35px] tablet:text-[40px] laptop:text-[50px] desktop:text-6xl extraLarge:text-7xl"
+                    >
                       {navLinkText[index].split("").map((e, i) => {
                         return (
                           <span
@@ -291,56 +302,54 @@ const Navbar = () => {
           </div>
 
           <span
-            className={`absolute w-25 h-25 rounded-full flex justify-center items-center text-2xl bg-cover bg-center overflow-hidden`}
+            className={`absolute top-1/2 w-25 h-25 rounded-full flex justify-center items-center text-2xl bg-cover bg-center overflow-hidden z-10`}
             id="Nav_Link_Circle"
             style={{
               backgroundImage: `url(/images/nav-image-${nav_Link_Circle_Text}.webp)`,
             }}
           >
             <span className="absolute inset-0 bg-black/50 backdrop-blur-[1px]" />
-            <span className="relative z-10 drop-shadow-lg  text-[#fdfcdc]">
+            <span className="relative drop-shadow-lg  text-[#fdfcdc]">
               {nav_Link_Circle_Text}
             </span>
           </span>
         </div>
 
         <div
-          className="w-full h-24 absolute bottom-0 text-[#fdfcdc] border-t border-dashed flex flex-col justify-between items-center p-1 backdrop-blur-sm"
+          className="w-full absolute bottom-0 text-[#fdfcdc] border-t border-dashed flex flex-wrap justify-center gap-5 items-center px-2 py-3 mobile:py-5 backdrop-blur-sm font-Bruney bg-[#0D1717] z-20"
           id="Nav_Bottom"
         >
-          <div className="flex flex-wrap justify-center gap-7 items-center w-full font-Bruney pt-4">
-            {navBottomLinkText.map((elem, ind) => {
-              return (
-                <React.Fragment key={ind}>
-                  <a
-                    href={elem.toLocaleLowerCase().replace(/[^a-zA-Z0-9]/g, "")}
-                    className="font-extralight text-lg tracking-widest relative inline-block overflow-hidden px-5 border-r border-dashed"
-                    onMouseEnter={() => {
-                      nav_Bottom_Links_Animation_Start(ind);
-                    }}
-                    onMouseLeave={() => {
-                      nav_Bottom_Links_Animation_Reset(ind);
-                    }}
-                    ref={(el) => {
-                      Nav_Bottom_Links_Ref.current[ind] = el;
-                    }}
+          {navBottomLinkText.map((elem, ind) => {
+            return (
+              <React.Fragment key={ind}>
+                <a
+                  // href={elem.toLocaleLowerCase().replace(/[^a-zA-Z0-9]/g, "")}
+                  className="font-extralight text-xs mobile:text-sm tablet:text-lg tracking-widest relative inline-block overflow-hidden px-3 mobile:px-5 border-r border-dashed"
+                  onMouseEnter={() => {
+                    nav_Bottom_Links_Animation_Start(ind);
+                  }}
+                  onMouseLeave={() => {
+                    nav_Bottom_Links_Animation_Reset(ind);
+                  }}
+                  ref={(el) => {
+                    Nav_Bottom_Links_Ref.current[ind] = el;
+                  }}
+                >
+                  <span className={`block Nav_Bottom_Link_1`}>{elem}</span>
+                  <span
+                    className={`absolute left-0 top-full Nav_Bottom_Link_2`}
                   >
-                    <span className={`block Nav_Bottom_Link_1`}>{elem}</span>
-                    <span
-                      className={`absolute left-0 top-full Nav_Bottom_Link_2`}
-                    >
-                      {elem}
-                    </span>
-                  </a>
-                </React.Fragment>
-              );
-            })}
-          </div>
+                    {elem}
+                  </span>
+                </a>
+              </React.Fragment>
+            );
+          })}
 
-          <p className="self-end text-sm copyright font-mono">
+          {/* <p className="self-center text-xs tablet:text-sm copyright font-mono">
             {" "}
             © {new Date().getFullYear()} Car X. All rights reserved.
-          </p>
+          </p> */}
         </div>
       </nav>
     </>
